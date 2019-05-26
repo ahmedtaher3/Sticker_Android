fdf156446e3ac71933deb9ba30f3f5110290dec2
ahmed <ahmedtaher376@gmail.com>
2019-05-26 04:51:41 +0200

    vote commit

--- app/src/main/java/com/sticker_android/fcm/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/sticker_android/fcm/MyFirebaseInstanceIDService.java
@@ -3,15 +3,21 @@ package com.sticker_android.fcm;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
 
 /**
  * Created by user on 14/11/17.
  */
 
-public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+public class MyFirebaseInstanceIDService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+    }
 
     /**
      * Called if InstanceID token is updated. This may occur if the security of
@@ -19,18 +25,7 @@ public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
      * is initially generated so this is where you would retrieve the token.
      */
     // [START refresh_token]
-    @Override
-    public void onTokenRefresh() {
 
-        // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-
-    }
 
 
     // [END refresh_token]
